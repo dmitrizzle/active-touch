48942cc6390e962dfd890bbf148662fae802bd91
Dmitri <d@eurekaking.com>
2017-05-23 14:50:31 +0700

    Converted objects to array, separated single-line statements

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-// REWRITE TOUCH AND CLICK ACTIVE STATES FOR ".active" CSS class
+// REWRITE TOUCH AND CLICK DOWNSTATE STATES FOR ".downstate" CSS class
 export const activeTouch = {
 	// sensitivity settings
 	options: {
-		touchMoveDelay: 200,			// wait this many ms to see if the user is about to scroll or tap the element
-		slack: 					35,				// allow dragging this many px before deactivating the element state
-		selectors: 			["a"],		// CSS selectors affected by this script
-		cssclass:				"active",	// default css class for active elements
+		touchMoveDelay: 200,					// wait this many ms to see if the user is about to scroll or tap the element
+		slack: 					35,						// allow dragging this many px before deactivating the element state
+		selectors: 			["a"],				// CSS selectors affected by this script
+		cssclass:				"downstate",	// default css class for downstate elements
 	},
 
-	// remove all other active states on the document
+	// remove all other downstate states on the document
 	reset: function(){
 		let opto = this.options
-		for (let el of document.querySelectorAll(opto.selectors)) el.classList.remove(opto.cssclass)
+		for (let el of [...document.querySelectorAll(opto.selectors)]) el.classList.remove(opto.cssclass)
 	},
 	
 	// add events to all elements on page
@@ -21,25 +21,25 @@ export const activeTouch = {
 			typeof options.cssclass === "string" ? this.options.cssclass = options.cssclass : null
 			typeof options.selectors !== "undefined" && options.selectors.constructor === Array ? this.options.selectors = options.selectors : null
 		} else 
-			throw Error("Wrong variable type passed as options. It needs to be an object in a form of { cssclass: 'value', selectors: ['value','..'] }");
+			throw Error("Wrong variable type passed as options. It needs to be an object in a form of { cssclass: 'value', selectors: ['value','..'] }")
+		
 		let opto = this.options
-				
-		for (let el of document.querySelectorAll(opto.selectors)) {
+		for (let el of [...document.querySelectorAll(opto.selectors)]) {
 			let pointerDown = 			false
-			let activeAvailable = 	true
+			let downstateAvailable = 	true
 			let pointerLocFirst = 	{}
 			let pointerLocLast = 		{}
 			let touchMoveTimer = 		-1
-		
+
 			let touchMoveDelay = 		opto.touchMoveDelay
 			let slack = 						opto.slack
 						
-			// add/remove active classes
+			// add/remove downstate classes
 			const activate = (el) 	=> el.classList.add(opto.cssclass)
-			const deactivate = (el) => { el.classList.remove(opto.cssclass); activeAvailable = false }
+			const deactivate = (el) => { el.classList.remove(opto.cssclass); downstateAvailable = false }
 		
 		
-			// allow dragging and still keeping element active for [slack] pixels
+			// allow dragging and still keeping element downstate for [slack] pixels
 			const outsideRange = (a, b) => {
 				const getX = (o) => (typeof o.clientX !== 'undefined') ?  o.clientX : o.pageX
 				const getY = (o) => (typeof o.clientY !== 'undefined') ?  o.clientY : o.pageY
@@ -51,13 +51,13 @@ export const activeTouch = {
 		
 			// activate down state
 			const downstate = (el, e) => {
-				activeAvailable = true
+				downstateAvailable = true
 				activate(el)
 				pointerDown = 		true
 				pointerLocFirst = e
 			}
 			const upstate = (el) => {
-				if(pointerDown && activeAvailable) {
+				if(pointerDown && downstateAvailable) {
 					this.reset()
 					activate(el)
 				}
@@ -67,23 +67,23 @@ export const activeTouch = {
 			el.addEventListener("mousedown", 	function(e){ downstate(this, e) })
 			el.addEventListener("mouseup", 		function( ){ upstate(this) })
 		
-			el.addEventListener("touchstart", function(e){ 
+			el.addEventListener("touchstart", function(e){
 				if (e.touches.length == 1){
-					activeAvailable = true
-					pointerDown = 		true
-					let el = 					this
+					downstateAvailable, pointerDown = true
+					let el = this;
+					
 					(touchMoveTimer != -1) ? clearTimeout(touchMoveTimer) : null
 
 					// timeout function that doesn't add the class until delay [touchMoveDelay] has passed
-					let touchMoveTimer = setTimeout(() => activeAvailable ? downstate(self, e.touches[0]) : null, touchMoveDelay)
+					let touchMoveTimer = setTimeout(() => downstateAvailable ? downstate(this, e.touches[0]) : null, touchMoveDelay)
 					
 				}
-			});
+			})
 			el.addEventListener("touchend", function(){ upstate(this) })
 		
 			// deactivate down state
 			const pointermove = (el, e) => {
-				if(activeAvailable){
+				if(downstateAvailable){
 					let pointerLocLast = e
 					if( pointerDown && outsideRange(pointerLocFirst, pointerLocLast) ) {
 						deactivate(el)
@@ -91,7 +91,7 @@ export const activeTouch = {
 					}
 				}
 			}
-			const pointerout = (el) => (pointerDown && activeAvailable) ? deactivate(el) : null
+			const pointerout = (el) => (pointerDown && downstateAvailable) ? deactivate(el) : null
 			el.addEventListener("mousemove", 		function(e){ pointermove	(this, e) })
 			el.addEventListener("mouseout", 		function( ){ pointerout		(this) })
 			el.addEventListener("touchmove", 		function(e){ pointermove	(this, e.targetTouches[0]) })
